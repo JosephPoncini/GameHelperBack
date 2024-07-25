410d025900cb346db7e729b9d918345a4ee7e6a0
JosephPoncini <jxponcini@gmail.com>
2024-07-25 12:11:28 -0700

    idk

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Desktop\CodeStack Academy\PassionProjects\GameHelperBack\venv
+set VIRTUAL_ENV=C:\Users\csa_jponcini\Desktop\Projects\Passion Project\GameHelperBack\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
