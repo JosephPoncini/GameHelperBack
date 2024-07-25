68e92cf8f78ecacedae452054fd5ecc84ad48256
JosephPoncini <jxponcini@gmail.com>
2024-07-25 12:04:33 -0700

    wordHunt Backend End Implemented

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from services.wordleService import get_wordle_data
+from services.wordHuntService import get_word_hunt_data
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -16,6 +17,13 @@ def wordle_get_data():
     code = data.get('code')
     word_bank = data.get('word_bank')
     result = get_wordle_data(word, code, word_bank)  # Process the data
+    return jsonify(result)  # Return the result as a JSON response
+
+@app.route('/api/word_hunt_get_data', methods=['POST'])
+def word_hunt_get_data():
+    data = request.get_json()  # Get JSON data from request body
+    board = data.get('board')
+    result = get_word_hunt_data(board)  # Process the data
     return jsonify(result)  # Return the result as a JSON response
 
 if __name__ == '__main__':
